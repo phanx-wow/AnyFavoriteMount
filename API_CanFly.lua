b401052feb0e6a380ef9f5895a8ef9a28ea3ad2d
Phanx <addons@phanx.net>
2015-01-03 23:31:04 -0800

    Update CanFly file

--- API_CanFly.lua
+++ API_CanFly.lua
@@ -1,4 +1,15 @@
+-- This file provides a single function addons can call to determine whether
+-- flying mounts can really be used in the player's current location. The API
+-- function IsFlyableArea is unusable for this purpose, as it not only reports
+-- the area is flyable if there are *any* circumstances under which the zone
+-- becomes flyable (eg. by reaching a higher level and buying an ability) but
+-- it also reports Draenor is flyable when there is not currently any way for
+-- players to gain the ability to fly there.
+-- Written by Phanx <addons@phanx.net> and released into the public domain.
+-- Feel free to use this file in your own addons!
+
 local _, ns = ...
+-- TODO: Find out when Wintergrasp isn't flyable? Or too old to bother with?
 
 local flyingSpell = {
 	[0]   = 90267,  -- Eastern Kingdoms = Flight Master's License
@@ -19,7 +30,9 @@ local flyingSpell = {
 	[1160] = -1, -- SMV Alliance Garrison Level 4
 }
 
-function ns.CanFly() -- because IsFlyableArea is a fucking liar
+local GetInstanceInfo, IsFlyableArea, IsSpellKnown = GetInstanceInfo, IsFlyableArea, IsSpellKnown
+
+function ns.CanFly()
 	if IsFlyableArea() then
 		local _, _, _, _, _, _, _, instanceMapID = GetInstanceInfo()
 		local reqSpell = flyingSpell[instanceMapID]
